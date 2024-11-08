873e64fdb4e6df64f8097cfa3563e5d98fafebb2
deividas strole <deiwis@yahoo.com>
2024-11-07 19:55:17 -0800

    git commit -m

--- src/main/java/com/coderscampus/assignment13/web/UserController.java
+++ src/main/java/com/coderscampus/assignment13/web/UserController.java
@@ -8,10 +8,13 @@ import com.coderscampus.assignment13.service.AddressService;
 import com.coderscampus.assignment13.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
@@ -73,10 +76,14 @@ public class UserController {
 
     @GetMapping("/users/{userId}")
     public String getOneUser(ModelMap model, @PathVariable Long userId) {
+
+
+
         User user = userService.findById(userId);
         Address address = addressService.getAddress(userId);
         List<Account> accounts = user.getAccounts();
 
+        model.addAttribute("password", "");
         model.put("accounts", accounts);
         model.put("address", address);
         model.put("users", Arrays.asList(user));
@@ -86,7 +93,10 @@ public class UserController {
     }
 
     @PostMapping("/users/{userId}")
-    public String postOneUser(User user, Address address, Account account, @PathVariable Long userId) {
+    public String postOneUser(User user, Address address, Account account, @PathVariable Long userId, @RequestParam("password") String password) {
+
+        System.out.println("*-*****************************************" + password);
+
         userService.saveUser(user);
         addressService.saveAddress(address);
 
